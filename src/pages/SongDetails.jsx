4d3bddb6582cffb9ec7edf7b0d5b85598c925ac9
Pedro Taciano <pedrotmonte@gmail.com>
2022-09-26 00:08:13 -0300

    fix logo & name bugs

--- src/pages/SongDetails.jsx
+++ src/pages/SongDetails.jsx
@@ -1,3 +1,27 @@
-const SongDetails = () => <div>SongDetails</div>;
+import { useParams } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { DetailsHeader, Error, Loader, RelatedSongs } from "../components";
+
+import { setActiveSong, playPause } from "../redux/features/playerSlice";
+
+const SongDetails = () => {
+    const { songid } = useParams();
+    const dispatch = useDispatch();
+    const { setActiveSong, isPlaying } = useSelector((state) => state.player);
+
+    return (
+        <div className="flex flex-col">
+            {/*<DetailsHeader artistId={artistId} songData={songData} /> */}
+
+            <div className="mb-10">
+                <h2 className="text-white text-3xl font-bold">Lyrics:</h2>
+                <div className="mt-5">
+                    
+                </div>
+            </div>
+        </div>
+    );
+
+}
 
 export default SongDetails;
