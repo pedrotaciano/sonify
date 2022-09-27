886280911e2b74d2974891da5b945eb6524c4e77
Pedro Taciano <pedrotmonte@gmail.com>
2022-09-27 01:39:51 -0300

    add search

--- src/components/ArtistCard.jsx
+++ src/components/ArtistCard.jsx
@@ -1,5 +1,14 @@
-const ArtistCard = () => {
-  <div>ArtistCard</div>;
+import { useNavigate } from "react-router-dom";
+
+const ArtistCard = ({ track }) => {
+  const navigate = useNavigate();
+
+  return (
+    <div className="flex flex-col w-[250px] p-4 bg-white/5 bg-opacity-80 backdrop-blur-sm rounded-lg cursor-pointer animate-slideup" onClick={() => navigate(`/artists/${track?.artists[0].adamid}`)}>
+      <img src={track?.images?.coverart} alt="artist" className="w-full h-56 rounded-lg"/>
+      <p className="truncate mt-4 font-semibold text-lg text-white">{track?.subtitle}</p>
+    </div>
+  )
 };
 
 export default ArtistCard;
--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,16 +1,18 @@
 import { Error, Loader, SongCard } from "../components";
 import { genres } from "../assets/constants";
-import{ useGetTopChartsQuery } from '../redux/services/shazamCore';
+import{ useGetSongsByGenreQuery } from '../redux/services/shazamCore';
 import { useDispatch, useSelector } from "react-redux";
+import { selectGenreListId } from "../redux/features/playerSlice";
 
 const Discover = () => {
-    const genreTitle = 'Pop';
-    const { data, isFetching, error } = useGetTopChartsQuery(genreTitle);
     const dispatch = useDispatch();
-    const { activeSong, isPlaying } = useSelector((state) => state.player);
-
+    const { activeSong, isPlaying, genreListId } = useSelector((state) => state.player);
+    const { data, isFetching, error } = useGetSongsByGenreQuery(genreListId || 'POP');
+    
     if(isFetching) return <Loader title="Loading songs..."/>;
     if(error) return <Error title="Something went wrong!"/>;
+    
+    const genreTitle = genres.find(({ value }) => value === genreListId)?.title;
 
     return (
         <div className="flex flex-col">
@@ -18,8 +20,8 @@ const Discover = () => {
                 <h2 className="font-bold text-3xl text-white text-left">Discover {genreTitle}</h2>
 
                 <select 
-                    onChange={() => {}}
-                    value=""
+                    onChange={(e) => dispatch(selectGenreListId(e.target.value))}
+                    value={genreListId}
                     className="bg-black text-gray-300 p-3 text-sm rounded-lg outline-none sm:mt-0 mt-5"
                     >
                     {genres.map((genre) => (<option key={genre.value} value={genre.value}>{genre.title}</option>))}
